9242224583358c45069b422919118fee356f2d22
ALIHSAN-SHOUROV <164044359+MOHAMMAD-SHOUROV@users.noreply.github.com>
2026-02-02 10:42:14 -0800

    Update errors.js

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -1,51 +1,51 @@
 const { ERROR_CODES } = require('./constants');
 
-class NeokexError extends Error {
+class ShourovError extends Error {
   constructor(message, code, details = {}) {
     super(message);
-    this.name = 'NeokexError';
+    this.name = 'ShourovError';
     this.code = code;
     this.details = details;
     Error.captureStackTrace(this, this.constructor);
   }
 }
 
-class LoginError extends NeokexError {
+class LoginError extends ShourovError {
   constructor(message, details = {}) {
     super(message, ERROR_CODES.LOGIN_FAILED, details);
     this.name = 'LoginError';
   }
 }
 
-class CheckpointError extends NeokexError {
+class CheckpointError extends ShourovError {
   constructor(message, details = {}) {
     super(message, ERROR_CODES.CHECKPOINT_REQUIRED, details);
     this.name = 'CheckpointError';
   }
 }
 
-class AccountLockedError extends NeokexError {
+class AccountLockedError extends ShourovError {
   constructor(message, details = {}) {
     super(message, ERROR_CODES.ACCOUNT_LOCKED, details);
     this.name = 'AccountLockedError';
   }
 }
 
-class AccountSuspendedError extends NeokexError {
+class AccountSuspendedError extends ShourovError {
   constructor(message, details = {}) {
     super(message, ERROR_CODES.ACCOUNT_SUSPENDED, details);
     this.name = 'AccountSuspendedError';
   }
 }
 
-class SessionExpiredError extends NeokexError {
+class SessionExpiredError extends ShourovError {
   constructor(message, details = {}) {
     super(message, ERROR_CODES.SESSION_EXPIRED, details);
     this.name = 'SessionExpiredError';
   }
 }
 
-class RateLimitError extends NeokexError {
+class RateLimitError extends ShourovError {
   constructor(message, details = {}) {
     super(message, ERROR_CODES.RATE_LIMIT, details);
     this.name = 'RateLimitError';
